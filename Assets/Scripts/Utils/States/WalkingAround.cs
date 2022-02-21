7490fb3a7f3a6102d9fb16019c366f244084cfa6
Cristian <cri.ghiorzi@gmail.com>
2022-02-21 14:02:52 +0100

    Update WalkingAround.cs

--- Assets/Scripts/Utils/States/WalkingAround.cs
+++ Assets/Scripts/Utils/States/WalkingAround.cs
@@ -1,26 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class WalkingAround : IState
 {
-    public WalkingAround()
-    {
+    private NavMeshAgent _agent;
+    private Transform[] _points;
 
+    public WalkingAround(NavMeshAgent agent, Transform[] points)
+    {
+        _agent = agent;
+        _points = points;
     }
 
     public void OnEnter()
     {
-        throw new System.NotImplementedException();
+        _agent.enabled = true;
+        _agent.isStopped = false;
+
+        int indexPoint = Random.Range(0, _points.Length);
+        _agent.SetDestination(_points[indexPoint].position);
     }
 
     public void OnExit()
     {
-        throw new System.NotImplementedException();
+        _agent.isStopped = true;
+        _agent.enabled = false;
     }
 
     public void Tick()
     {
-        throw new System.NotImplementedException();
+        float dist = _agent.remainingDistance;
+        if (dist != Mathf.Infinity && _agent.pathStatus == NavMeshPathStatus.PathComplete && dist == 0)
+        {
+            // Arrived
+            int indexPoint = Random.Range(0, _points.Length); // Select the next point
+            _agent.SetDestination(_points[indexPoint].position); // Go to the next point
+        }
     }
 }
