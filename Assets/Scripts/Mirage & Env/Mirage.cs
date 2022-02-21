5e2226f24d4097884c0174df7a91138eeaccda78
Cristian <cri.ghiorzi@gmail.com>
2022-02-21 12:03:25 +0100

    Mirage script fixed

--- Assets/Scripts/Mirage & Env/Mirage.cs
+++ Assets/Scripts/Mirage & Env/Mirage.cs
@@ -2,36 +2,42 @@ using UnityEngine;
 
 public class Mirage : MonoBehaviour
 {
-    [SerializeField] private float _externalRadius; // Mirage Appears when player enters
+    //[SerializeField] private float _externalRadius; // Mirage Appears when player enters
     [SerializeField] private float _internalRadius; // Mirage Disappears when player enters
 
     public LayerMask whatIsPlayer;
 
     [SerializeField] private GameObject mirageObject; // Forest, Arctic, etc
 
+    private bool _itAppeared = false;
+
     private void Update()
     {
-        if (Physics.CheckSphere(transform.position, _externalRadius, whatIsPlayer))
-        {
+        //if (Physics.CheckSphere(transform.position, _externalRadius, whatIsPlayer))
+        //{
             if (Physics.CheckSphere(transform.position, _internalRadius, whatIsPlayer))
             {
-                RemovesMirage();
+                //RemovesMirage();
+                ShowMirage();
             }
             else
             {
-                ShowMirage();
+                //ShowMirage();
+                RemovesMirage();
             }
-        }
+        //}
     }
 
     private void ShowMirage()
     {
+        _itAppeared = true;
         mirageObject.SetActive(true);
     }
 
     private void RemovesMirage()
     {
         mirageObject.SetActive(false);
-        Destroy(gameObject);
+        if(_itAppeared)
+            Destroy(gameObject);
     }
 }
