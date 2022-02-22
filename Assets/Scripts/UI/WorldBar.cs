0fcf69303ddcc46ff9164a23d48dbedadfd3bb70
Kateryna Kvasniak <kateryna.kvasniak@gmail.com>
2022-02-22 16:01:01 +0200

    World canvas for health of enemies & animals

---
+++ Assets/Scripts/UI/WorldBar.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldBar : MonoBehaviour
+{
+    void Update()
+    {
+        this.transform.rotation = Quaternion.identity;
+    }
+}
