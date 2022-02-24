28ea8213418a873185cddb559a344bc82f250df2
Cristian <cri.ghiorzi@gmail.com>
2022-02-24 10:03:52 +0100

    basic animal and enemies spawner

--- Assets/Scripts/Utils/Spawners/EnemySpawner.cs
+++ Assets/Scripts/Utils/Spawners/EnemySpawner.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AnimalSpawner : Spawner
+public class EnemySpawner : Spawner
 {
     [SerializeField]
     private float spawnRate;
