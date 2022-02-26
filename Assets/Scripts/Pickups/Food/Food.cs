382864f0e56a2d1d3e8ac90a33b96ec3e755c524
Aidan Moisan <68043386+Acemoisan@users.noreply.github.com>
2022-02-26 14:21:43 -0700

    Few animal spawners / baked Navmesh

--- Assets/Scripts/Pickups/Food/Food.cs
+++ Assets/Scripts/Pickups/Food/Food.cs
@@ -5,17 +5,32 @@ using UnityEngine;
 public class Food : MonoBehaviour, IPickable
 {
     [SerializeField] private FoodData _food;
+    bool _allowedToPickup = false;
 
+    private void OnEnable()
+    {
+        Invoke("TriggerDelay", 2);
+    }
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
-            PickUp(collision.gameObject);
+
+        if (_allowedToPickup)
+        {
+            if (collision.gameObject.CompareTag("Player"))
+                PickUp(collision.gameObject);
+        }
+        else { return; }
     }
 
+    void TriggerDelay()
+    {
+        _allowedToPickup = true;
+    }
 
     public void PickUp(GameObject player)
     {
         player.GetComponent<Player>().AddFoodAmount(_food.Value);
+        CancelInvoke();
         Destroy(gameObject);
     }
 }
